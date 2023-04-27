6a614964c4f12bea3c2cdefedfe006291b3298bd
Felix Garduza <felixgarduza@hotmail.com>
2023-04-27 00:15:45 -0600

    Login de facebook a medias

--- app/src/main/java/com/veseguro/veseguroprototype/InicioSesion.java
+++ app/src/main/java/com/veseguro/veseguroprototype/InicioSesion.java
@@ -1,5 +1,6 @@
 package com.veseguro.veseguroprototype;
 
+import static com.veseguro.veseguroprototype.ProviderType.FACEBOOK;
 import static com.veseguro.veseguroprototype.ProviderType.GOOGLE;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,12 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -23,17 +30,27 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthCredential;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class InicioSesion extends AppCompatActivity {
 
     Button btnToMap;
     Button btnLoginGoogle;
+    Button btnLoginFacebook;
 
     // ID to pick up results from starting google login intent
     private final int GOOGLE_LOGIN_INTENT_ID = 100;
+
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
     final static String packagename = "com.veseguro.veseguroprototype";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +64,9 @@ public class InicioSesion extends AppCompatActivity {
         setContentView(R.layout.activity_inicio_sesion);
 
         // Declare buttons
-        btnToMap        = (Button)findViewById(R.id.IniciarSesion);
-        btnLoginGoogle  = (Button)findViewById(R.id.btnLoginGoogle);
+        btnToMap            = (Button)findViewById(R.id.IniciarSesion);
+        btnLoginGoogle      = (Button)findViewById(R.id.btnLoginGoogle);
+        btnLoginFacebook    = (Button)findViewById(R.id.btnLoginFacebook);
 
         // Button Listeners
         btnToMap.setOnClickListener(new View.OnClickListener() {
@@ -74,9 +92,44 @@ public class InicioSesion extends AppCompatActivity {
                 startActivityForResult(googleClient.getSignInIntent(), GOOGLE_LOGIN_INTENT_ID);
             }
         });
+
+        // Login con Facebook
+        btnLoginFacebook.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // Configuración
+                //<String> perms = Arrays.asList("email");
+                //LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email"));
+                LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        if(loginResult != null){
+                            AccessToken token = loginResult.getAccessToken();
+                            AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+                            firebaseLoginWithCredential(credential, GOOGLE);
+                        }
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        showLoginErrorAlert(FACEBOOK);
+                    }
+                });
+            }
+        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        // Desencadenar llamada a callbacks de Facebook
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
         super.onActivityResult(requestCode, resultCode, data);
 
         // Get results for Google Login
@@ -86,24 +139,15 @@ public class InicioSesion extends AppCompatActivity {
                 GoogleSignInAccount account = (GoogleSignInAccount) task.getResult(ApiException.class);
                 if(account != null) {
                     AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener() {
-                        @Override
-                        public void onComplete(@NonNull Task task) {
-                            if(task.isSuccessful()) {
-                                Intent i = new Intent();
-                                i.setClassName(packagename, "com.veseguro.veseguroprototype.Mapa");
-                                startActivity(i);
-                                return;
-                            }
-                            showLoginErrorAlert(GOOGLE);
-                        }
-                    });
+                    firebaseLoginWithCredential(credential, GOOGLE);
                 }
             } catch (Throwable e) {
                 showLoginErrorAlert(GOOGLE);
                 throw new RuntimeException(e);
             }
         }
+
+        // Get results for Google Login
     }
 
     private void showLoginErrorAlert(ProviderType provider){
@@ -118,5 +162,26 @@ public class InicioSesion extends AppCompatActivity {
                 break;
         }
         Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+    }
+
+    private String firebaseLoginWithCredential(AuthCredential credential, ProviderType provider) {
+        if (credential == null)
+            return null;
+
+        final Boolean[] loginSucessful = {false};
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                loginSucessful[0] = true;
+                if (task.isSuccessful()) {
+                    Intent i = new Intent();
+                    i.setClassName(packagename, "com.veseguro.veseguroprototype.Mapa");
+                    startActivity(i);
+                    return;
+                }
+                showLoginErrorAlert(provider);
+            }
+        });
+        return loginSucessful[0] ? "EXITO" : null;
     }
 }
